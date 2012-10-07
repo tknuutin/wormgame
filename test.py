8e13ff37357a5d2e178e1613bf3d0078c337affa
Miikka Salmi <mitusalmi@gmail.com>
2012-10-07 21:50:45 +0300

    pikku muokkausta

--- test.py
+++ test.py
@@ -2,28 +2,25 @@ import pygame
 import sys
 
 print "hello wormgame"
+SCREEN_SIZE = SCREEN_WIDTH, SCREEN_HEIGHT = 1024, 768
 
 def main():
     start()
 
 def start():
     pygame.init()
-    size = width, height = 1024, 768
 
     #screen is the lowermost layer of the screen
-    screen = pygame.display.set_mode(size)
+    screen = pygame.display.set_mode(SCREEN_SIZE)
     clock = pygame.time.Clock()
 
     #mapscreen is the layer for all the non-UI elements
-    mapscreen = pygame.Surface((1024, 768))
+    mapscreen = pygame.Surface(SCREEN_SIZE)
     maprect = mapscreen.get_rect()
 
-    black = 0, 0, 0
-    mapscreen.fill(black)
+    mapscreen.fill(pygame.Color("black"))
 
     while True:
-        #limit fps
-        clock.tick(30)
 
         #copy mapscreen contents to main screen
         screen.blit(mapscreen, maprect)
@@ -33,6 +30,9 @@ def start():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+        
+        #limit fps
+        clock.tick(30)
 
 if __name__ == "__main__":
     main()
