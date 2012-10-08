56536ca81b646c980cd4c6bdefe7936d79aeb783
Lasokki <lasokki@gmail.com>
2012-10-08 13:38:08 +0300

    Added utf-8 -coding

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import argparse, ConfigParser, os
 
 class Config:
@@ -15,12 +16,12 @@ class Config:
         self.config_file_args = self.parse_config_file()
         
     def parse_cmdline_args(self):
-        parser = argparse.ArgumentParser(description='Configurate the Wormgame.')
-        parser.add_argument('--host', type=bool, help='Is the client host or not (True/False)')
-        parser.add_argument('--ip', help='listen in IP')
-        parser.add_argument('--port', type=int, help='listen in PORT')
-        parser.add_argument('--name', help='NAME of the server')
-        parser.add_argument('--players', type=int, help='number of players')
+        parser = argparse.ArgumentParser(prog='Wormgame', description='Configurate the Wormgame.')
+        parser.add_argument('-host', choices='01', help='Is the client host or not (1/0)')
+        parser.add_argument('-ip', help='listen in IP')
+        parser.add_argument('-port', type=int, help='listen in PORT')
+        parser.add_argument('-name', help='NAME of the server')
+        #parser.add_argument('-players', type=int, choices=range(1,65), help='number of players')
     
         args = parser.parse_args()
         return args
@@ -37,15 +38,22 @@ class Config:
         return conf_dict
         
     def parse_config_file(self):
-        parser = ConfigParser.ConfigParser()
+        parser = ConfigParser.SafeConfigParser()
         parser.readfp(open('wormgame.ini'))
-        conf_dict = dict(parser.items("settings"))
-        return conf_dict
+        
+        conf_dict = {}
+        items = ['network', 'keys']
+        
+        for i in items:
+            conf_dict.update(parser.items(i))
+        
+        #return conf_dict
+        return parser
         
     
 if __name__ == "__main__":
     # Test code
     conf = Config()
     print vars(conf.cmd_line_args)
-    print conf.config_file_args
+    print conf.config_file_args.get('keys', 'left')
     
